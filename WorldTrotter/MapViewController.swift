3173c7fa5752441d98afd68ac637b29e24f946df
James <james@honeyball.com>
2017-01-29 23:11:49 +0000

    Using manual auto-layout constraints

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -6,12 +6,38 @@
 //  Copyright © 2017 James Honeyball. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
-class MapViewCOntroller: UIViewController {
+class MapViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("MapViewController loaded it's view.")
     }
     
+    var mapView: MKMapView!
+
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: 8)
+        
+        let margins = view.layoutMarginsGuide
+        
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+    }
 }
